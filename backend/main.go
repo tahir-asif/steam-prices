18b2a4f942b459b4d7c1ec5d2f44bb86023bebab
Tahir Asif <asift2@mcmaster.ca>
2026-02-21 11:59:34 -0500

    Fix: handle error from running router

--- backend/main.go
+++ backend/main.go
@@ -21,12 +21,16 @@ func main() {
         MaxAge:           12 * time.Hour,
     }))
 
+    // health check
     router.GET("/api/health", func(ginContext *gin.Context) {
         ginContext.JSON(http.StatusOK, gin.H{
             "status": "ok",
         })
     })
 
+    // runs the server
     log.Println("Server is running on http://localhost:3000")
-    router.Run(":3000")
+    if err := router.Run(":3000"); err != nil {
+        log.Fatalf("Failed to start server: %v", err)
+    }
 }
